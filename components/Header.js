ece1b727a2876be15f0dba573e9311a4fabc79e2
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-18 16:43:26 +0200

    feat: Refactor to func, useRouter for active page

--- components/Header.js
+++ components/Header.js
@@ -1,44 +1,51 @@
-import React, { Component } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const linkStyle = {
   marginRight: 15,
 };
 
-class Header extends Component {
-  render() {
-    const { title, description } = this.props;
+const Header = (props) => {
+  const router = useRouter();
+  const { title, description } = props;
 
-    return (
-      <>
-        <div className="left">
-          <Link href="/">
-            <a title={title} style={linkStyle} className="site_title">
-              {title}
-            </a>
-          </Link>
-          {description && <span className="site_description">{description}</span>}
-        </div>
-        <div className="right">
-          <Link href="/">
-            <a title="Home Page" style={linkStyle}>
-              Home
-            </a>
-          </Link>
-          <Link href="/about">
-            <a title="About Page" style={linkStyle}>
-              About
-            </a>
-          </Link>
-          <Link href="/posts">
-            <a title="Blog Page" style={linkStyle}>
-              Blog
-            </a>
-          </Link>
-        </div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div className="left">
+        <Link href="/">
+          <a title={title} style={linkStyle}>
+            {title}
+          </a>
+        </Link>
+        {description && <span className="site_description">{description}</span>}
+      </div>
+      <div className="right">
+        <Link href="/">
+          <a title="Home Page" style={linkStyle} className={router.pathname == "/" ? "active" : ""}>
+            Home
+          </a>
+        </Link>
+        <Link href="/about">
+          <a
+            title="About Page"
+            style={linkStyle}
+            className={router.pathname == "/about" ? "active" : ""}
+          >
+            About
+          </a>
+        </Link>
+        <Link href="/posts">
+          <a
+            title="Blog Page"
+            style={linkStyle}
+            className={router.pathname == "/posts" ? "active" : ""}
+          >
+            Blog
+          </a>
+        </Link>
+      </div>
+    </>
+  );
+};
 
 export default Header;
