7c389451a61693afdd3a50c3eca3ef649f5d02c1
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-19 17:23:47 +0200

    feat: Refactor to class, call dynamic menu

--- components/Header.js
+++ components/Header.js
@@ -1,51 +1,60 @@
+import React, { Component } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { withRouter } from "next/router";
+
+import config from "./../config";
+
+const { api } = config;
 
 const linkStyle = {
   marginRight: 15,
 };
 
-const Header = (props) => {
-  const router = useRouter();
-  const { title, description } = props;
+class Header extends Component {
+  state = {
+    menu: [],
+  };
 
-  return (
-    <>
-      <div className="left">
-        <Link href="/">
-          <a title={title} style={linkStyle}>
-            {title}
-          </a>
-        </Link>
-        {description && <span className="site_description">{description}</span>}
-      </div>
-      <div className="right">
-        <Link href="/">
-          <a title="Home Page" style={linkStyle} className={router.pathname == "/" ? "active" : ""}>
-            Home
-          </a>
-        </Link>
-        <Link href="/about">
-          <a
-            title="About Page"
-            style={linkStyle}
-            className={router.pathname == "/about" ? "active" : ""}
-          >
-            About
-          </a>
-        </Link>
-        <Link href="/posts">
-          <a
-            title="Blog Page"
-            style={linkStyle}
-            className={router.pathname == "/posts" ? "active" : ""}
-          >
-            Blog
-          </a>
-        </Link>
-      </div>
-    </>
-  );
-};
+  async componentDidMount() {
+    // TODO: Move menus call to _app
+    const res = await fetch(`${api}/menus/v1/menus/primary`);
+    const data = await res.json();
 
-export default Header;
+    this.setState({ menu: data.items });
+  }
+
+  render() {
+    const { router, title, description } = this.props;
+    const { menu } = this.state;
+    return (
+      <>
+        <div className="left">
+          <Link href="/">
+            <a title={title} style={linkStyle}>
+              {title}
+            </a>
+          </Link>
+          {description && <span className="site_description">{description}</span>}
+        </div>
+        <div className="right">
+          {menu.length > 0 &&
+            menu.map((item) => {
+              return (
+                <Link href={`/${item.url.split("/")[3]}`} key={item.ID}>
+                  <a
+                    title={item.title}
+                    style={linkStyle}
+                    className={router.pathname == `/${item.slug}` ? "active" : ""}
+                  >
+                    {item.title}
+                  </a>
+                </Link>
+              );
+            })}
+        </div>
+      </>
+    );
+  }
+}
+
+export default withRouter(Header);
